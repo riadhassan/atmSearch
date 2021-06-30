286c3fec37053d62769649bb209c5a5d7940d25b
Riad <riad_hassan@outlook.com>
2021-07-01 01:56:44 +0600

    add search functionality

--- atm_search/views.py
+++ atm_search/views.py
@@ -1,9 +1,16 @@
 from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
+from data.atms import get_locations
 
 
 def hello_world(request: HttpRequest):
-    context = {"message": "Allah is almighty"}
+    query_location = request.GET.get("location")
+    if query_location:
+        atms = filter(lambda atm: query_location.lower() in (atm.address+atm.location).lower(), get_locations())
+    else:
+        atms = get_locations()
+
+    context = {"atm_lists": atms}
     rendered = render(request, "index.html", context)
     return HttpResponse(rendered)
 
