1b95ebabdaac95384dd163cd69cf64a1039e11a8
Md. Riad Hassan <riad_hassan@outlook.com>
2021-07-05 23:22:21 +0600

    Update settings.py

--- atm_search/settings.py
+++ atm_search/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bqhxae4jz^mz!w@5gdu0r190g8psj17^$8ai2s_&srbfesp!ms'
+SECRET_KEY = 'Your_Secret_Key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
